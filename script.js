6bab0146917825b6160e11bbc2cbb00b07411079
kiiinhos <viniciuscamargo713@gmail.com>
2019-07-19 16:41:00 -0300

    alterei o embaralhar

--- script.js
+++ script.js
@@ -1,24 +1,33 @@
-const quadrados = document.querySelectorAll('.card');
+quadrados = document.querySelectorAll('.card');
 function virarCard(){
     this.classList.toggle('flip');
+    
     // console.log('fui clicado');
     // console.log(this);
 }
 quadrados.forEach(quadrado => quadrado.addEventListener('click', virarCard));
 // colocamos efeito de FLIP nas cartas
 
-// function listaCartas (quadrados){
-//     let numeroCartas= quadrados[i];
-//     let embaralhar= Math.floor(Math.random()*i);
 
-// //     let numeroCartas;
-// //     let embaralhar;
 
-// for(let i = quadrados.length - 1; i !== 0; i --){
-//     quadrados[i]= quadrados[embaralhar];
-//     quadrados[embaralhar] = numeroCartas;
-// }
 
-// console.log(quadrados)
-// return quadrados;
-// }
+// Manter a Funtion embatalhar dessa forma, estou corrigindo erros.
+function embaralhar (quadrados){
+    var numeroCartas= quadrados.length;
+    var embaralharCartas = 0; 
+    var i;
+while(0 !== numeroCartas){
+       i= Math.floor(Math.random()* numeroCartas);
+    numeroCartas -= 1;
+    embaralharCartas= quadrados[numeroCartas];
+    quadrados[numeroCartas] = quadrados[i];
+    quadrados[i] = embaralharCartas;
+console.log()   
+}
+
+console.log()
+return quadrados;
+}
+embaralhar(quadrados);
+// Não apresenta erros no Console, mas não esta embaralhando as Cartas. Não esquecer de verificar os parametos !
+

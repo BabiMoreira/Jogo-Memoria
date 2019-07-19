fe4d7856f0cb38ba389954414781351e1c9fad89
Rosangela C. Prieto <prietoisabele@gmail.com>
2019-07-19 20:37:43 -0300

    trocar as variaveis de var para let

--- script.js
+++ script.js
@@ -13,20 +13,20 @@ quadrados.forEach(quadrado => quadrado.addEventListener('click', virarCard));
 
 // Manter a Funtion embatalhar dessa forma, estou corrigindo erros.
 function embaralhar (quadrados){
-    var numeroCartas= quadrados.length;
-    var embaralharCartas = 0; 
-    var i;
+    let numeroCartas= quadrados.length;
+    let embaralharCartas = 0; 
+    let i;
 while(0 !== numeroCartas){
        i= Math.floor(Math.random()* numeroCartas);
     numeroCartas -= 1;
     embaralharCartas= quadrados[numeroCartas];
     quadrados[numeroCartas] = quadrados[i];
     quadrados[i] = embaralharCartas;
-console.log()   
+console.log(embaralhar)   
 }
 
 console.log()
-return quadrados;
+return quadrados;''
 }
 embaralhar(quadrados);
 // Não apresenta erros no Console, mas não esta embaralhando as Cartas. Não esquecer de verificar os parametos !

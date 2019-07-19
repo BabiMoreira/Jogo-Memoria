3385159cb7413d5e91abef251b60478af6c5b392
BabiMoreira <babisantos631@gmail.com>
2019-07-19 13:12:06 -0300

    fazer function

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ function virarCard(){
     // console.log(this);
 }
 quadrados.forEach(quadrado => quadrado.addEventListener('click', virarCard));
+// colocamos efeito de FLIP nas cartas
 
+// function listaCartas (quadrados){
+//     let numeroCartas= quadrados[i];
+//     let embaralhar= Math.floor(Math.random()*i);
 
+// //     let numeroCartas;
+// //     let embaralhar;
 
+// for(let i = quadrados.length - 1; i !== 0; i --){
+//     quadrados[i]= quadrados[embaralhar];
+//     quadrados[embaralhar] = numeroCartas;
+// }
+
+// console.log(quadrados)
+// return quadrados;
+// }

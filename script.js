a97fea891bd4c78d52c4cfa928b9f6bddd790beb
Rosangela C. Prieto <prietoisabele@gmail.com>
2019-07-19 21:41:30 -0300

    tentar embaralhar

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
+let img = ['img1.jpg','img2.jpg','img3.jpg','img4.jpg','img5.jpg','img6.jpg']
+
+let imagens = img.concat(img)
+
+let card1 
+let card2
+img = document.querySelectorAll ('img')
+
+
 quadrados = document.querySelectorAll('.card');
 function virarCard(){
     this.classList.toggle('flip');
-    
+
+    classList.toggle(img)    
+
     // console.log('fui clicado');
     // console.log(this);
 }
 quadrados.forEach(quadrado => quadrado.addEventListener('click', virarCard));
 // colocamos efeito de FLIP nas cartas
 
-
-
-
 // Manter a Funtion embatalhar dessa forma, estou corrigindo erros.
-function embaralhar (quadrados){
-    let numeroCartas= quadrados.length;
+
+function embaralhar (quadrados2){
+    let numeroCartas= quadrados2.length;
     let embaralharCartas = 0; 
     let i;
 while(0 !== numeroCartas){
        i= Math.floor(Math.random()* numeroCartas);
     numeroCartas -= 1;
-    embaralharCartas= quadrados[numeroCartas];
-    quadrados[numeroCartas] = quadrados[i];
-    quadrados[i] = embaralharCartas;
-console.log(embaralhar)   
+    embaralharCartas= quadrados2[numeroCartas];
+    quadrados2[numeroCartas] = quadrados2[i];
+    quadrados2[i] = embaralharCartas;
+    console.log (embaralharCartas)
+}
+return quadrados2;
 }
 
-console.log()
-return quadrados;''
-}
-embaralhar(quadrados);
+embaralhar(img)
+
+
+// embaralhar(imagens);
 // Não apresenta erros no Console, mas não esta embaralhando as Cartas. Não esquecer de verificar os parametos !
 

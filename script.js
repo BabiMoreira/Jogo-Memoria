3dcea549470559a1c2120c33d36921adfeebed03
kiiinhos <viniciuscamargo713@gmail.com>
2019-07-21 20:44:22 -0300

    terminei de arrumar as funcoes

--- script.js
+++ script.js
@@ -1,148 +1,134 @@
+//Lista com caminho para imagens dos gatos
 let imagensGatos = [
   "img/img1.jpg",
   "img/img2.jpg",
   "img/img3.jpg",
   "img/img4.jpg",
   "img/img5.jpg",
-  "img/img6.jpg",
+  "img/img6.jpg"
 ];
+
+//lista das DIVs que representam as cartas
 let cartas = document.querySelectorAll(".card");
+//Primeira carta a ser selecionada
 let carta1;
+//Segunda carta a ser selecionada
 let carta2;
+
+//Duplico a lista de imagens de gatos (imagensGatos)
 let imagens = imagensGatos.concat(imagensGatos);
 
-// Cria uma lista com dias copias das imagens
-imagens = embaralhar(imagens);
-// Inicio do jogo
-for(carta of cartas){
-  abrir(carta);
-}
-setTimeout(function(){
-  iniciarJogada();
+//Função que embaralha a lista de imagens (já duplicadas)
+const cartasEmbaralhadas = embaralhar(imagens);
 
-},2000);
-// Função recebe Vetor com uma lista vazia e embaralha as cartas
+//Função que insere as imagens embaralhadas nas cartas
+inserirImagemNaCarta();
+// Função de Iniciar
+play();
 
-function embaralhar(vetor){
-  let lugar;
-  let posicao;
-  
-  
-  for(let variavel = vetor.length- 1; variavel !== 0; variavel--){
-    posicao= Math.floor(Math.random()*variavel);
+function embaralhar(listaImagens) {
+  // criei duas variaveis indefinidas
+  let imagemSelecionada;
+  let posicaoAleatoria;
+  // Faz um LOOP na lista para embaralhar
+  for (let posicao = listaImagens.length - 1; posicao !== 0; posicao--) {
+    posicaoAleatoria = Math.floor(Math.random() * posicao);
 
-    lugar = vetor[variavel];
-    vetor[variavel]= vetor[posicao];
-    vetor[posicao] = lugar;
+    imagemSelecionada = listaImagens[posicao];
+    listaImagens[posicao] = listaImagens[posicaoAleatoria];
+    listaImagens[posicaoAleatoria] = imagemSelecionada;
   }
-  console.log(vetor);
-  mandarCartas(vetor);
-}
 
-function mandarCartas(lista){
-  
-  for (i = 0; i<lista.length;i++){
-    document.querySelectorAll('img')[i].src = lista[i];
-    console.log(document.querySelectorAll('img')[i]);
+  return listaImagens;
+}
+// Faço um LOOP nas divs e insiro as imagens em cada uma, usando o style e backGroundImage
+function inserirImagemNaCarta() {
+  for (let carta of cartas) {
+    carta.style.backgroundImage = `url('${
+      cartasEmbaralhadas[Number(carta.id)]
+    }')`;
   }
 }
-
-// Funções para mostrar frente e verso da carta
-function fecharCarta(face){
-  face.style.backgroundImage= "url'('img/itachi.jpeg')"
-  face.onclick= clique;
+// Faço um LOOP nas divs e insiro as imagens em cada uma, usando o style e backGroundImage
+function inserirImagemDeVerso() {
+  for (let carta of cartas) {
+    carta.style.backgroundImage = `url('img/itachi.jpeg')`;
+    // Adiciono o evento clik em todas as cartas
+    adicionarEventoDeClick(carta);
+  }
+}
+//Adiciono o evento clik em todas as cartas
+function adicionarEventoDeClick(carta) {
+  // Verifico se a carta é TRUE
+  if (carta) {
+    carta.addEventListener("click", function(event) {
+      cartasCorretas(event);
+    });
+  }
 }
 
-function abrirCarta(face){
-  face.style.backgroundImage= `url('img/${img[Number(carta.id)]}')`;
-  carta.onclick= null ;
-  
+// Funções para mostrar o verso da carta
+function fecharCarta(carta) {
+  carta.style.backgroundImage = "url('img/itachi.jpeg')";
+  carta.onclick = null;
 }
-// Funções para mostrar frente e verso da carta
+// Função para mostrar a Frente da carta
+function abrirCarta(carta) {
+  carta.style.backgroundImage = `url('${imagens[Number(carta.id)]}')`;
+  carta.onclick = null;
+}
 
 // Faz as cartas não funcionar com o click
-function cartaParacima(){
-  for(let carta of cartas){
-    carta.onclick= null;
+function cartaParacima() {
+  for (let carta of cartas) {
+    carta.onclick = null;
   }
 }
 // Função fecha todas as cartas erradas
-function cartaParabaixo(){
-  for(let carta of cartas){
-    if(!carta.classList.contains('certa')){
+function cartaParabaixo() {
+  for (let carta of cartas) {
+    if (!carta.classList.contains("certa")) {
       fecharCarta(carta);
     }
   }
 }
 
-// Compara se as cartas são iguais
-function cartasCorretas(event){
+// Indentificar as cartas selecionadas, usando 2 funções
+function cartasCorretas(event) {
   abrirCarta(event.target);
-  if(carta1){
-    carta2= event.target;
+  if (carta1) {
+    carta2 = event.target;
     cartaParacima();
     verificarCartas();
-  }
-  else{
-    carta1= event.target;
+  } else {
+    carta1 = event.target;
   }
 }
-// Função compara as cartas iguais, se for igual trava as 2 para cima 
-function verificarCartas(){
-  if(carta1.style.backgroundImage!==carta2.style.backgroundImage){
-    setTimeout(function(){
+// Função compara as cartas iguais, se for igual trava as 2 para cima
+function verificarCartas() {
+  if (carta1.style.backgroundImage !== carta2.style.backgroundImage) {
+    setTimeout(function() {
       fecharCarta(carta1);
       fecharCarta(carta2);
-      comecarJogo();
+      confereCartas();
     }, 2000);
-  }
-  else{
-    carta1.classList.add('certa');
-    carta2.classList.add('certa');
-    comecarJogo();
+  } else {
+    carta1.classList.add("certa");
+    carta2.classList.add("certa");
+    confereCartas();
   }
 }
-// Função para iniciar a jogada
-function comecarJogo(){
-  carta1= null;
-  carta2=null;
+// Confiro apos cada jogada
+function confereCartas() {
+  carta1 = null;
+  carta2 = null;
   cartaParabaixo();
 }
-// // console.log(img);
-
-// function virarCard() {
-//   this.classList.toggle("flip");
-//   classList.toggle();
-// }
-// quadrados.forEach(quadrado => quadrado.addEventListener("click", virarCard));
-// // colocamos efeito de FLIP nas cartas
-
-// // Fun
-
-// function embaralhar(cards) {
-//   let tamanhoCards = cards.length - 1;
-//   let indiceAleatorio;
-//   let valorTemporario;
-
-//   cards.forEach((card, position) => {
-//     if (tamanhoCards !== 0) {
-//       indiceAleatorio = Math.floor(Math.random() * tamanhoCards);
-//       valorTemporario = cards[position];
-//       cards[position] = cards[indiceAleatorio];
-//       cards[indiceAleatorio] = valorTemporario;
-//     }
-//   });
-
-//   return cards;
-// }
-
-// // const emba =  quadrados[index].getElementById("card").children.length;
-// const cardsEmbaralhados = embaralhar(imagens);
-// quadrados.forEach((val, index) => {
-//   cardsEmbaralhados.forEach(caminhoImagem => {
-    
-//     quadrados[index].children[0].src = caminhoImagem;  });
-
-//     console.log(val.children[0].src);
-
-// });
+// Função iniciar o jogo
+function play() {
+  setTimeout(function() {
+    inserirImagemDeVerso();
+    cartaParabaixo();
+    adicionarEventoDeClick();
+  }, 2000);
+}
